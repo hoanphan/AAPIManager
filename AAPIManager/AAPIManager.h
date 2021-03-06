5af926e3f5746f7b7b64e7cce29dc4e8a74532c0
hoandhtb <phanvanhoan54cntt@gmail.com>
2021-03-06 15:16:12 +0700

    Initial Commit

---
+++ AAPIManager/AAPIManager.h
@@ -0,0 +1,19 @@
+//
+//  AAPIManager.h
+//  AAPIManager
+//
+//  Created by HOANDHTB on 3/6/21.
+//  Copyright © 2021 HOANDHTB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AAPIManager.
+FOUNDATION_EXPORT double AAPIManagerVersionNumber;
+
+//! Project version string for AAPIManager.
+FOUNDATION_EXPORT const unsigned char AAPIManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAPIManager/PublicHeader.h>
+
+
